03e76bada53b7d66c8d758d26ab56308c490667f
Harlan Heath <harlandheath@gmail.com>
2018-11-10 13:18:41 -0600

    updating nav bar and moved assets folder

--- sim-1-async/src/Components/NavBar/Nav-Bar.js
+++ sim-1-async/src/Components/NavBar/Nav-Bar.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "../../assets/logo.png";
 import "./Nav-Bar.css";
 
 export default function NavBar() {
   return (
     <div className="nav-bar">
       <div className="nav-container">
-        <img />
+        <img src="../../assets/logo.png" />
         <h1 className="shelfie">SHELFIE</h1>
       </div>
     </div>
